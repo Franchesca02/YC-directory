23374a32a12ec3f4a26cd18f62b5c245a7104529
franchesca <franchesca>
2025-04-10 15:53:54 +0100

    fixed src attribute

--- app/(root)/user/[id]/page.tsx
+++ app/(root)/user/[id]/page.tsx
@@ -9,33 +9,38 @@ import { Suspense } from 'react';
 
 export const experimental_ppr = true;
 
-const page  = async ({ params }: { params: Promise<{ id: string }> }) => {
-    const id = (await params).id;
-    const session = await auth();
-  
-    const user = await client.fetch(AUTHOR_BY_ID_QUERY, { id });
-    if (!user) return notFound();
+const page = async ({ params }: { params: Promise<{ id: string }> }) => {
+  const id = (await params).id;
+  const session = await auth();
 
+  const user = await client.fetch(AUTHOR_BY_ID_QUERY, { id });
+  if (!user) return notFound();
 
-    
+  // Provide default values for potentially null fields
+  const userImage = user.image || '/default-avatar.png';
+  const userName = user.name || 'Anonymous';
+  const username = user?.username || 'user';
+  const bio = user?.bio || 'No bio available';
+
   return (
     <>
-    <section className='profile_container'>
+      <section className='profile_container'>
         <div className='profile_card'>
-            <div className='profile_title'>
-        <h3 className='text-24-black uppercase text-center line-clamp-1'>
-            {user.name}
-        </h3>
-            </div>
-            <Image
-            src={user.image}
-            alt={user.name}
+          <div className='profile_title'>
+            <h3 className='text-24-black uppercase text-center line-clamp-1'>
+              {userName}
+            </h3>
+          </div>
+          <Image
+            src={userImage}
+            alt={`Profile picture of ${userName}`}
             width={220}
             height={220}
             className='profile_image'
-            />
-            <p className='text-30-extrabold text-center mt-7'>@{user?.username}</p>
-            <p className='text-14-normal mt-1 text-center'>{user?.bio}</p>
+            priority
+          />
+          <p className='text-30-extrabold text-center mt-7'>@{username}</p>
+          <p className='text-14-normal mt-1 text-center'>{bio}</p>
         </div>
 
         <div className="flex-1 flex flex-col gap-5 lg:-mt-5">
@@ -48,10 +53,9 @@ const page  = async ({ params }: { params: Promise<{ id: string }> }) => {
             </Suspense>
           </ul>
         </div>
-
-    </section>
+      </section>
     </>
   )
 }
 
-export default page
+export default page;
